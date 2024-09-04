8d1560e791060b2d321223f0e245b82884953099
JuanDiegoG <jdiegogarcia0110@gmail.com>
2024-09-04 13:42:35 -0500

    GVM-5 Se crea el componente de home totalmente responsive y se agrega el title y el icon

---
+++ src/components/home/home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './home.css';
+import logo from '../../images/LogoConNombre.png'
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="overlay">
+                <img src={logo} alt="Logo" className="logo" />
+                <p>Bienvenido a la</p>
+                <h1>Granja Agroturistica Villa Maria </h1>
+                <p>Estamos encantados de tenerte aquí. Explora nuestras ofertas y servicios.</p>
+                <a href="https://wa.me/3138832314?text=Hola%20me%20gustaría%20saber%20más%20sobre%20sus%20servicios..." target="_blank" rel="noopener noreferrer">
+                    <button className="whatsapp-button">Habla con nosotros</button>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
